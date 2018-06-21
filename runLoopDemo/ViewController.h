60f3fcdf29acf40ef72bcc55003d4c1721389834
Your Name <you@example.com>
2018-06-21 10:24:46 +0800

    Initial Commit

---
+++ runLoopDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  runLoopDemo
+//
+//  Created by Cary on 2018/6/21.
+//  Copyright © 2018年 Cary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ runLoopDemoTests/runLoopDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  runLoopDemoTests.m
+//  runLoopDemoTests
+//
+//  Created by Cary on 2018/6/21.
+//  Copyright © 2018年 Cary. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface runLoopDemoTests : XCTestCase
+
+@end
+
+@implementation runLoopDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
